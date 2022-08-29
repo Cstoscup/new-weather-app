af345c4b169b59260cb2ee4685a88277b2caa3b6
Cstoscup <109521820+Cstoscup@users.noreply.github.com>
2022-08-29 07:31:12 -0400

    added code link and linkedin link t bttom

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <Weather defaultCity="New York" />
+      <p><a href="https://github.com/Cstoscup/new-weather-app" target="_blank">Open-source</a> code, by <a href='https://www.linkedin.com/in/callie-stoscup-475b1683/' target='_blank'>Callie Stoscup</a></p>
     </div>
   );
 }
